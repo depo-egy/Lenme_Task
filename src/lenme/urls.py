d1b364b45c82408628ccb453c5da7a3e62924b83
Mohammad Diab <32489701+depo-egy@users.noreply.github.com>
2021-06-19 20:56:33 +0200

    Update urls.py

--- src/lenme/urls.py
+++ src/lenme/urls.py
@@ -6,7 +6,7 @@ app_name = 'lenme'
 urlpatterns = [
     path('api/request-loan',views.BorrowerCreateView.as_view(), name='loan_request'),
     path('api/make-offer', views.InvestorCreateOffer.as_view(), name='make_offer'),
-    path('api/accept-offer', views.BorrowerAcceptOffer.as_view(), name='clients'),
-    path('clients/', views.FinalizeOffer.as_view(), name='clients'),
+    path('api/accept-offer', views.BorrowerAcceptOffer.as_view(), name='accept_offer'),
+    path('api/finalize-loan', views.FinalizeOffer.as_view(), name='finalize_loan'),
     
 ]
